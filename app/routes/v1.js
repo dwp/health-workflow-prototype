4a9f183c02728bc929aa3e03ac40373aa3579d52
Sean Regan <seanregan@engineering.digital.dwp.gov.uk>
2025-08-21 16:49:20 +0100

    chore: task updates
    GitOrigin-RevId: 1f2452a09d24e9aca842cac7cf7aa34371384d4b

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -6,10 +6,24 @@
 
 module.exports = function(router) {
 
-router.get("/test/", (req, res) => {
-  res.redirect('https://www.bbc.co.uk/news')
+
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/v1/task-type', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var taskType = req.session.data['task-type']
+
+  // Check whether the variable matches a condition
+  if (taskType == "hcp-review"){
+    // Send user to next page
+    res.redirect('/v1/task-summary/hcp-review')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/v1/task-summary/hcp-review')
+  }
+
 })
- 
 
 } // end module.exports
  
